4f7ee3787130d6fc3a22755f5ca71ba4cbc05310
Prashant Mishra <92492422+DevloperJI@users.noreply.github.com>
2025-04-24 17:22:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "solo-leveling-level-up", // ← replace with your actual repo name if different
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
